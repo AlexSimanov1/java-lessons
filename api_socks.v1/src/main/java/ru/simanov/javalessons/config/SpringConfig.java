ac4ca0d694078cfe57ebdc0e841fef5c3b31dfa7
AlexSimanov1 <Alex0104-94@mail.ru>
2021-12-06 13:05:17 +0300

    Socks. hibernate

--- api_socks.v1/src/main/java/ru/simanov/javalessons/config/SpringConfig.java
+++ api_socks.v1/src/main/java/ru/simanov/javalessons/config/SpringConfig.java
@@ -14,4 +14,5 @@ public class SpringConfig implements WebMvcConfigurer {
     public MethodValidationPostProcessor methodValidationPostProcessor() {
         return new MethodValidationPostProcessor();
     }
+
 }
